674fb5dec9d7e05531558e2254818f4982217082
kingwangwong <kingngngco@gmail.com>
2023-02-16 15:00:10 -0600

    seeddata

---
+++ db/seedData.js
@@ -0,0 +1,41 @@
+const cars = [
+    {
+        make: "Honda",
+        model: "Civic",
+        year: "2018",
+        mileage:40000,
+        price: 15000,
+    },
+    {
+        make: "Cadillac",
+        model: "CTS",
+        year: "2016",
+        mileage:9000,
+        price: 50000,
+    },
+    {
+        make: "Toyota",
+        model: "Highlander",
+        year: "2005",
+        mileage:140000,
+        price: 4000,
+    },
+    {
+        make: "Hyundai",
+        model: "Veloster",
+        year: "2020",
+        mileage:10000,
+        price: 28000,
+    },
+    {
+        make: "Chevy",
+        model: "Spark",
+        year: "2006",
+        mileage:93000,
+        price: 7000,
+    },
+];
+
+module.exports = {
+    cars
+}
