5311c595be952a96a12247281799903ebf428e78
Kharmalina Tong <kharmalinat7@gmail.com>
2023-03-06 17:17:05 -0500

    add test comment

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+// comment
 const auth0Domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
