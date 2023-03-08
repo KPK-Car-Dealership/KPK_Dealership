93d429210b9bc5fffa70d1ac841ac38583d0b5c8
Patrick Borgella <patrickborgella1@gmail.com>
2023-03-08 15:18:30 -0600

    Resolve merge conflict

--- client/App.js
+++ client/App.js
@@ -4,10 +4,6 @@ const JWT_TOKEN = process.env.REACT_APP_JWT_TOKEN;
 
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
-const JWT_TOKEN = process.env.REACT_APP_JWT_TOKEN;
-
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
 
 function App() {
   const [carsList, setCarsList] = useState([]);
@@ -42,36 +38,29 @@ function App() {
       <h1>Cars Cars come get ya cars!!</h1>
       {carsList.map((car, idx) => (
         <div>
-          <h2>Make:{car.make}- Color:{car.color}- Mileage:{car.mileage}- Model:{car.model}- Price:{car.price}</h2>
+          <h2>
+            Make:{car.make}- Color:{car.color}- Mileage:{car.mileage}- Model:
+            {car.model}- Price:{car.price}
+          </h2>
           <img src={car.image} alt="car" width="250" height="250"></img>
         </div>
       ))}
     </>
-
   );
 }
 
 export { App };
 
-
-    // <FavoritesProvider>
-    //     <BrowserRouter>
-    //         <div data-testid="app">
-    //             <Navigation />
-    //             <Routes>
-    //                 {/* <Route path="/" element={<Home userList={userList} userFilteredList={userFilteredList} setUserFilteredList={setUserFilteredList}/>} />
-    //                 <Route path="/characters/:_id" element={<UserDetails userFilteredList={userFilteredList}/>} />
-    //                 <Route path="/favorites" element={<FavCharacters />} /> */}
-    //                 <Route path="/" />
-    //             </Routes>
-    //         </div>
-    //     </BrowserRouter>
-    // </FavoritesProvider>
-    <div>
-      <h1>Hello is this working???</h1>
-      <LoginButton />
-    </div>
-  );
-}
-
-export { App };
+// <FavoritesProvider>
+//     <BrowserRouter>
+//         <div data-testid="app">
+//             <Navigation />
+//             <Routes>
+//                 {/* <Route path="/" element={<Home userList={userList} userFilteredList={userFilteredList} setUserFilteredList={setUserFilteredList}/>} />
+//                 <Route path="/characters/:_id" element={<UserDetails userFilteredList={userFilteredList}/>} />
+//                 <Route path="/favorites" element={<FavCharacters />} /> */}
+//                 <Route path="/" />
+//             </Routes>
+//         </div>
+//     </BrowserRouter>
+// </FavoritesProvider>
