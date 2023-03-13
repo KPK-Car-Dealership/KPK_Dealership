1c44aaf83d41b85915deec2b91d64c83f8dbe2fb
Kharmalina Tong <kharmalinat7@gmail.com>
2023-03-13 16:44:29 -0400

    initial connection of login code successful

--- client/App.js
+++ client/App.js
@@ -7,41 +7,56 @@ import Navbar from "./components/NavBar";
 import Login from "./components/buttons/Login";
 
 function App() {
-  const [carsList, setCarsList] = useState([]);
+  const [carsList, setCarsList] = useState(null);
   const [carsFilteredList, setCarsFilteredList] = useState([]);
   const [token, setToken] = useState("");
 
 
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("http://localhost:3000/cars", {
-        headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJwYXRyaWNrYm9yZ2VsbGExIiwibmFtZSI6IlBhdHJpY2sgQm9yZ2VsbGEgSnIiLCJwYXNzd29yZCI6bnVsbCwiZW1haWwiOiJwYXRyaWNrYm9yZ2VsbGExQGdtYWlsLmNvbSIsImNyZWF0ZWRBdCI6IjIwMjMtMDMtMDIgMTc6NDg6NDEuNTgwICswMDowMCIsInVwZGF0ZWRBdCI6IjIwMjMtMDMtMDIgMTc6NDg6NDEuNTgwICswMDowMCIsImlhdCI6MTY3ODM4MzMxMSwiZXhwIjoxNjc4OTg4MTExfQ.hlyEkf37biSiCS7VRavS-c14m1ornBHQ1QGqTml_234`,
-        },
-      }).then(async (response) => {
-        const data = await response.json();
-        console.log(data);
-        setCarsList(data);
-        setCarsFilteredList(data);
-      });
-    };
-    // console.log(JWT_TOKEN)
-    fetchData().catch(console.error);
-    console.log(carsList)
-  }, []);
-
   // useEffect(() => {
-  //   if(token) {
-  //     console.log("isfgrg");
-  //   }
-  // }, [token])
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:3000/cars", {
+  //       headers: {
+  //         Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJwYXRyaWNrYm9yZ2VsbGExIiwibmFtZSI6IlBhdHJpY2sgQm9yZ2VsbGEgSnIiLCJwYXNzd29yZCI6bnVsbCwiZW1haWwiOiJwYXRyaWNrYm9yZ2VsbGExQGdtYWlsLmNvbSIsImNyZWF0ZWRBdCI6IjIwMjMtMDMtMDIgMTc6NDg6NDEuNTgwICswMDowMCIsInVwZGF0ZWRBdCI6IjIwMjMtMDMtMDIgMTc6NDg6NDEuNTgwICswMDowMCIsImlhdCI6MTY3ODM4MzMxMSwiZXhwIjoxNjc4OTg4MTExfQ.hlyEkf37biSiCS7VRavS-c14m1ornBHQ1QGqTml_234`,
+  //       },
+  //     }).then(async (response) => {
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setCarsList(data);
+  //       setCarsFilteredList(data);
+  //     });
+  //   };
+  //   // console.log(JWT_TOKEN)
+  //   fetchData().catch(console.error);
+  //   console.log(carsList)
+  // }, []);
+
+  useEffect(() => {
+    if(token) {
+      const fetchData = async () => {
+        const res = await fetch("http://localhost:3000/cars", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then(async (response) => {
+          const data = await response.json();
+          console.log(data);
+          setCarsList(data);
+          setCarsFilteredList(data);
+        });
+      };
+      // console.log(JWT_TOKEN)
+      fetchData().catch(console.error);
+      console.log(carsList)
+      console.log(token);
+    }
+  }, [token])
 
     return (
       <>       
         <main>
           {
-            !token ? (
+            !carsList ? (
               <Login setToken={setToken} token={token}/>
             ) : 
               <div>
